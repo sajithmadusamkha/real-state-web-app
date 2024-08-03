de3cd8f055023d88503d203b41bc2b1ef28d7ba4
sajithmadusamkha <sajithmadusankha02@gmail.com>
2024-08-03 23:07:44 +0530

    project uploaded

---
+++ app/page.js
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import Link from "next/link";
+import { Flex, Box, Text, Button } from "@chakra-ui/react";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Hello World</h1>
+    </div>
+  );
+}
