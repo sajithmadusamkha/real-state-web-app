dbd4ecc43f4aff068440ea65fbc93c3bfc411350
sajithmadusamkha <sajithmadusankha02@gmail.com>
2024-08-14 21:02:01 +0530

    fetch api file added

---
+++ app/utils/fetchApi.js
@@ -0,0 +1,11 @@
+import axios from "axios";
+
+export const baseUrl = "https://bayut.p.rapidapi.com";
+export const fetchApi = async (url) => {
+  const { data } = await axios.get(url, {
+    headers: {
+      "x-rapidapi-key": "0ca6aa9adamshc1787ccaaa012dap167384jsne2f8436a182b",
+      "x-rapidapi-host": "bayut.p.rapidapi.com",
+    },
+  });
+};
--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    domains: ["bayut-production.s3.eu-central-1.amazonaws.com"],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
